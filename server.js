51dd9f58dc3fabfe84a4f96ca331d21434b376f5
Paul Marley <p.marley1993@gmail.com>
2022-04-21 11:02:04 -0500

    update

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ require("dotenv").config();
 const PORT = process.env.port || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
-  app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "./client/build/index.html"));
-  });
+  app.use(express.static("client/build"));
+  // app.get("*", (req, res) => {
+  //   req.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  // });
 }
 
 const validateEmail = (email) => {
