0493e91d872ada3a1f88eba64945300938a44f68
Paul Marley <p.marley1993@gmail.com>
2022-04-21 11:19:31 -0500

    update port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 require("dotenv").config();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
