421786d517fae457cb77aeba7f2c27576059b5ff
Paul Marley <p.marley1993@gmail.com>
2022-07-29 17:35:34 +0100

    fix iphone blur

--- client/src/components/About/Chart/ChartButtons.tsx
+++ client/src/components/About/Chart/ChartButtons.tsx
@@ -51,10 +51,10 @@ export default function ChartButtons({
               : "rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px",
         }}
       >
-        <Box>{i.icon}</Box>
-        <Heading ml={2} fontSize={["12px", "14px", "14px", "16px"]}>
-          {i.name}
-        </Heading>
+        <Box mr={2} display={["none", "flex", "flex", "flex"]}>
+          {i.icon}
+        </Box>
+        <Heading fontSize={["12px", "14px", "14px", "16px"]}>{i.name}</Heading>
       </motion.button>
     </GridItem>
   ));
